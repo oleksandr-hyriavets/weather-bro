23e858fadfa17611e4de85ec04db998803a0cc46
Oleksandr Hyriavets <oleksandr.hyriavets@depositphotos.com>
2022-07-08 17:15:58 +0200

    Port

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import Router from "@koa/router";
 
 import { dailyForecastController } from "./controllers/daily-forecast.controller.js";
 
+const PORT = process.env.PORT || 5000;
+
 const app = new Koa();
 const router = new Router();
 
 router.get("/daily-forecast", dailyForecastController);
 
-app.use(router.routes()).use(router.allowedMethods()).listen(3000);
+app.use(router.routes()).use(router.allowedMethods()).listen(PORT);
