3c40d33bceed9ff80e54bb1cc86e4d813433646c
Oleksandr Hyriavets <oleksandr.hyriavets@depositphotos.com>
2023-04-23 22:06:41 +0200

    Create seperate class for main methods

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,32 @@ import Router from "@koa/router";
 import { dailyInfoController } from "./controllers/daily-info.controller";
 import { ConfigService } from "./services/config.service";
 
-const PORT = ConfigService.get('PORT') || 5000;
+class DailyInfo {
+    port = ConfigService.get('PORT') || 5000;
+
+    constructor(
+        private router: Router<Koa.DefaultState, Koa.DefaultContext>,
+        private app: Koa<Koa.DefaultState, Koa.DefaultContext>,
+    ) {}
+
+    defineEndpoints(): void {
+        this.router.get("/daily-info", dailyInfoController);
+    }
+
+    init(): void {
+        const handleListening = () => console.log(`Listening on port ${this.port}`);
+
+        this.app.use(this.router.routes())
+            .use(this.router.allowedMethods())
+            .listen(this.port)
+            .on('listening', handleListening);
+    }
+}
 
 const app = new Koa();
 const router = new Router();
 
-router.get("/daily-info", dailyInfoController);
+const dailyInfo = new DailyInfo(router, app);
 
-app.use(router.routes()).use(router.allowedMethods()).listen(PORT).on('listening', () => console.log(`Listening on port ${PORT}`));
+dailyInfo.defineEndpoints();
+dailyInfo.init();
