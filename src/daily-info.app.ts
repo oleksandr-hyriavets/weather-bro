28cce63043e51ae95e6a1250bcf6adc944f19cba
Oleksandr Hyriavets <oleksandr.hyriavets@depositphotos.com>
2023-05-17 19:13:22 +0200

    Move main class to seperate file

---
+++ src/daily-info.app.ts
@@ -0,0 +1,27 @@
+import Koa from "koa";
+import Router from "@koa/router";
+
+import { dailyInfoController } from "./controllers/daily-info.controller";
+import { ConfigService } from "./services/config.service";
+
+export class DailyInfo {
+    port = ConfigService.get('PORT') || 5000;
+
+    constructor(
+        private router: Router<Koa.DefaultState, Koa.DefaultContext>,
+        private app: Koa<Koa.DefaultState, Koa.DefaultContext>,
+    ) {}
+
+    defineEndpoints(): void {
+        this.router.get("/daily-info", dailyInfoController);
+    }
+
+    start(): void {
+        const handleListening = () => console.log(`Listening on port ${this.port}`);
+
+        this.app.use(this.router.routes())
+            .use(this.router.allowedMethods())
+            .listen(this.port)
+            .on('listening', handleListening);
+    }
+}
